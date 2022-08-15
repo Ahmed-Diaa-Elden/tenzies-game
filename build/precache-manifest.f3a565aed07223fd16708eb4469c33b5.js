3641bfe1c86dd4a8ae003ae6392b73d855bd380a
Ahmed-Diaa-Elden <56769369+Ahmed-Diaa-Elden@users.noreply.github.com>
2022-08-15 17:39:41 +0200

    Uploading build folder

---
+++ build/precache-manifest.f3a565aed07223fd16708eb4469c33b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9bb07c5438ac7dd2d39",
+    "url": "/static/css/main.46126b65.chunk.css"
+  },
+  {
+    "revision": "c9bb07c5438ac7dd2d39",
+    "url": "/static/js/main.c9bb07c5.chunk.js"
+  },
+  {
+    "revision": "a88627aa3cc12e58cd00",
+    "url": "/static/js/1.a88627aa.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5f6929f10a904c201e4bf905c3b034a1",
+    "url": "/index.html"
+  }
+];
